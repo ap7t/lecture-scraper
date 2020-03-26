ae8368359e2e07063b07b1e0f1c437d0533cb60b
ap7t <43987730+ap7t@users.noreply.github.com>
2020-03-26 22:10:39 +0000

    Update lecture_scraper.py

--- lecture_scraper.py
+++ lecture_scraper.py
@@ -15,7 +15,10 @@ class LectureScraper():
         try:
             self.driver = webdriver.Chrome(cwd / 'chromedriver', options=options)
         except:
-            print('Please save the chromedriver executable in this directory')
+            try:
+                self.driver = webdriver.Chrome(cwd / 'chromedriver.exe', options=options)
+            except:
+                print('Please save the chromedriver executable in this directory')
             quit()
         self.driver.get('https://ucc.instructure.com/')
         self.login(student_num, password)
